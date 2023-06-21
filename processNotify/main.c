6e164daf0b2fc1a51fd889a508489bbb2779820b
Karuulme <64870233+Karuulme@users.noreply.github.com>
2023-06-21 10:30:04 +0300

    initiated processes

---
+++ processNotify/main.c
@@ -0,0 +1,30 @@
+#include "ntddk.h"
+#include "wdm.h"
+
+VOID PcreateProcessNotifyRoutineEx(PEPROCESS Process,HANDLE ProcessId,PPS_CREATE_NOTIFY_INFO CreateInfo){
+	if (CreateInfo) {
+		DbgPrint("[Process Create] %i \r\n", ProcessId);
+		CreateInfo->CreationStatus = STATUS_ACCESS_DENIED;
+	}
+}
+
+VOID Unload(PDRIVER_OBJECT driverObject) {
+
+	DbgPrint("UNLOAD DRIVER");
+	
+}
+NTSTATUS DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING registryPath) {
+	
+	NTSTATUS status;
+	DbgPrint("LOAD DRIVER");
+	driverObject->DriverUnload = Unload;
+	
+	status = PsSetCreateProcessNotifyRoutineEx(PcreateProcessNotifyRoutineEx,FALSE);
+	if (!NT_SUCCESS(status)) {
+		DbgPrint("LOAD DRIVER BOOOOOSSSS");
+		return status;
+	}
+	return STATUS_SUCCESS;
+
+
+}
