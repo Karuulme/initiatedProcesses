a02e6c586f09e327383da350353ed3f10e921b0d
Karuulme <64870233+Karuulme@users.noreply.github.com>
2023-06-21 15:40:00 +0300

    .

--- processNotify/main.c
+++ processNotify/main.c
@@ -4,13 +4,15 @@
 VOID PcreateProcessNotifyRoutineEx(PEPROCESS Process,HANDLE ProcessId,PPS_CREATE_NOTIFY_INFO CreateInfo){
 	if (CreateInfo) {
 		DbgPrint("[Process Create] %i \r\n", ProcessId);
-		CreateInfo->CreationStatus = STATUS_ACCESS_DENIED;
+		//CreateInfo->CreationStatus = STATUS_ACCESS_DENIED;
+		CreateInfo->CreationStatus = STATUS_SUCCESS;
 	}
 }
 
 VOID Unload(PDRIVER_OBJECT driverObject) {
 
 	DbgPrint("UNLOAD DRIVER");
+	PsSetCreateProcessNotifyRoutineEx(PcreateProcessNotifyRoutineEx, TRUE);
 	
 }
 NTSTATUS DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING registryPath) {
